20d16189650bd473453ddd9a1ea4e7b7be969568
radwaahmed2132000 <56734728+radwaahmed2132000@users.noreply.github.com>
2021-05-16 01:43:36 +0200

    Initial MongoDB file

--- Project/src/MongoDB.java
+++ Project/src/MongoDB.java
@@ -5,14 +5,52 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.*;
 import org.bson.Document;
+
+import jdk.tools.jaotc.ELFMacroAssembler;
+
 import java.util.ArrayList;  
 import java.util.Arrays;  
 import java.io.File;
 import java.io.IOException; 
 import java.util.*;
+
 public class MongoDB {
     // ...
+	private MongoCollection<Document> collection;
+ public MongoDB( MongoCollection<Document> coll)
+   {
+	  collection=coll;
 
+   }
+   Boolean FindWord(String word)
+   {
+
+	Document dc=  collection.find(new Document("id",word)).first();
+   if(!dc.isEmpty())
+    return true;
+
+	return false;
+   }
+	 
+    void insertword(String word, int position ,int DocNumber)
+	{
+	
+
+      if( !FindWord(word))
+	{
+		Document Doc1=new Document("id",word);
+		Doc1.append(String.valueOf(DocNumber),position);
+		List<Document> list = new ArrayList<Document>();
+		list.add(Doc1);
+       collection.insertOne(Doc1);
+	}
+	else
+	{
+
+	}
+	   
+
+	}
 	public	static void main(String [] argv)
   {
 	ConnectionString connString = new ConnectionString(
@@ -26,16 +64,11 @@ public class MongoDB {
 	MongoClient mongoClient = MongoClients.create(settings);
 	MongoDatabase database = mongoClient.getDatabase("try");
 	MongoCollection<Document> collection = database.getCollection("first");
-			System.out.println("Collection sampleCollection selected successfully");
-			Document document1 = new Document("title", "MongoDB")
-			.append("description", "database")
-			.append("likes", 100)
-			.append("url", "http://www.tutorialspoint.com/mongodb/")
-			.append("by", "tutorials point");
-			List<Document> list = new ArrayList<Document>();
-			list.add(document1);
-			
-			collection.insertMany(list);
+		
+			MongoDB Mon=new MongoDB(collection);
+		
+		Mon.insertword("iiii",2,3);
+		
 	System.out.println("Collection sampleCollection selected successfully");
   }   
 }
